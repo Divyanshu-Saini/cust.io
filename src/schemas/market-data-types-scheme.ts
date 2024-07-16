c985c6a1cfd8d6cff56a4bd81c7abad8883073eb
Utkarsh Agarwal <utkarsha@valuefy.com>
2024-07-16 18:18:23 +0530

    Added schemas and interfaces/types for corporate action

--- src/schemas/market-data-types-scheme.ts
+++ src/schemas/market-data-types-scheme.ts
@@ -36,13 +36,38 @@ export type SchemeNavResponse = {
 
 ///////////////////////////////////////
 
-export interface SecurityPrices extends PageInfo {
-  PRICEDATE: string;
-  SECURITYCODE: string;
-  ISINCODE: string;
-  NSECLOSEPRICE: number;
-  BSECLOSEPRICE: number;
-  Description: string;
+export interface CorporateAction extends PageInfo {
+  RowId: number;
+  CAType: string;
+  BCNumber: string;
+  CompanyCode: number;
+  SecurityCode: string;
+  BSENSECode: string;
+  ISINCode: string;
+  BCFromDate: string;
+  BCToDate: string;
+  ExDate: string;
+  Remarks: string;
+  DividendRate: number;
+  DividendPerShare: number;
+  AddUpdFlag: string;
+  NewRatio: number;
+  OldRatio: number;
+  IssuePrice: number;
+  SubscriptionStartDate: number | null;
+  SubscriptionEndDate: number | null;
+  Notes: string;
+  Exchange: string;
+  EffectiveDate: string;
+  OldISIN: string;
+  OldName: string;
+  PayDate: number | null;
+  FaceValue: number;
+  MarketLot: number;
+  GROUP: string;
+  SecurityType: string;
+  SettlementMode: string;
+  LastModified: string;
 }
 
 export interface PageInfo {
@@ -56,8 +81,8 @@ export interface PageInfo {
   NEXT_PAGE: number;
 }
 
-export type SecurityPricesResponse = {
-  result: SecurityPrices[];
+export type CorporateActionResponse = {
+  result: CorporateAction[];
   previous: number | null;
   current: number;
   next: {
