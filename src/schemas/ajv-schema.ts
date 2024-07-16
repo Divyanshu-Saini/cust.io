f0aab0a0da87ef5763be01cf2f87be682ceb48ac
ARenoyK <austin.k2109@gmail.com>
2024-07-16 17:04:52 +0400

    made changes to schema for correct formatting

--- src/schemas/ajv-schema.ts
+++ src/schemas/ajv-schema.ts
@@ -1,7 +1,7 @@
-import schema from "./env-schemas"
-
 export const ajv_schema = {
     "type": "array",
+    "items": {
+    "type": "object",
     "properties": {
         "Scheme_Id": {"type": "number"},
         "Scheme_Name": {"type": "string"},
@@ -56,5 +56,40 @@ export const ajv_schema = {
         "Exit_Remarks": {"type": "string"},
         "AUM": {"type": "number"}
     }
-}
+    }
+};
 
+export const security_master = {
+    "type": "array",
+    "items": {
+    "type": "object",
+    "properties": { 
+        "CompanyCode": {"type": "number"},
+        "SecurityTypeCode": {"type": "number"},
+        "SecuritySubTypeCode": {"type": "number"},
+        "SerialNumber": {"type": "number"},
+        "SecurityCode": {"type": "string"},
+        "TickerName": {"type": "string"},
+        "IssuePrice": {"type": "number"},
+        "Description": {"type": "string"},
+        "DateOfIssue": {"type": "string", "format": "date-time"},
+        "DateOfExpiry": {"type": ["null", "string"]},
+        "FaceValue": {"type": "number"},
+        "MarketLot": {"type": "number"},
+        "ISINCode": {"type": "string"},
+        "Remarks": {"type": "string"},
+        "CreatedOn": {"type": "string"},
+        "CreatedBy": {"type": "string"},
+        "LastModifiedOn": {"type": "string"},
+        "LastModifiedBy": {"type": "string"},
+        "ReasonForModification": {"type": "string"},
+        "DeleteFlag": {"type": "boolean"},
+        "Code": {"type": "string"},
+        "IsItActiveSecurity": {"type": "boolean"},
+        "BroadIndustryCode": {"type": "number"},
+        "BroadIndustryName": {"type": "string"},
+        "ShortCompanyName": {"type": "string"},
+        "nsecode": {"type": ["null", "string"]}
+    }
+}
+}
