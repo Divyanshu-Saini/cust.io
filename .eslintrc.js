5879431d3b88ad5ec529dfee037b0ee70cfe3439
Divyanshu Saini <divyanshus@valuefy.com>
2024-07-17 11:39:13 +0530

    added new scripts in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module',
-        project: './tsconfig.json',
-    },
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
 
-    plugins: ['@typescript-eslint', 'prettier'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    rules: {
-        'prettier/prettier': [
-            'error',
-           { "usePrettierrc" : true }
-        ],
-        "@typescript-eslint/no-unused-vars": "off"
-        // Customize your rules here
-    },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['error', { usePrettierrc: true }],
+    '@typescript-eslint/no-unused-vars': 'off',
+    // Customize your rules here
+  },
 };
