a7954b553c85ce59362e51ba69c7e43ab85ef3f6
Divyanshu Saini <divyanshus@valuefy.com>
2024-08-02 07:26:38 +0530

    removed un used package

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,24 +1,13 @@
 import { FastifyInstance } from 'fastify';
-import { MSSQLFastifyInterface } from 'fastify-mssql';
 import { Server, Socket } from 'socket.io';
 declare module 'fastify' {
   interface FastifyInstance {
     config: {
       PORT: string;
-      MSSQL_SERVER: string;
-      MSSQL_USER: string;
-      MSSQL_PWD: string;
-      MSSQL_PORT: string;
-      MAX_POOL_SIZE: string;
-      MIN_POOL_SIZE: string;
-      DB_FUNDOO: string;
-      DB_VF_CC: string;
       MONGO_USER: string;
       MONGO_PWD: string;
       MONGO_DB: string;
     };
-    vfCc: MSSQLFastifyInterface;
-    fundoo: MSSQLFastifyInterface;
     io: Server;
   }
 }
